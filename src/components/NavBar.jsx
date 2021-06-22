db8ae8422ec229e5b058cd8bcfa8a14c6a3fbaaf
Josef Kaplan <joes.kaplan@gmail.com>
2021-06-22 13:26:46 +0200

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,22 +8,22 @@ import {makeStyles} from "@material-ui/core/styles";
 const styles = makeStyles({
     bar:{
         paddingTop: "1.15rem",
-        backgroundColor: "#fff",
+        backgroundColor: "#fff",// eslint-disable-next-line
         ['@media (max-width:780px)']: { 
            flexDirection: "column"
           }
     },
     logo: {
-        width: "15%", 
+        width: "15%", // eslint-disable-next-line
         ['@media (max-width:780px)']: { 
            display: "none"
            }
     },
     logoMobile:{
         width: "100%", 
-        display: "none", 
+        display: "none", // eslint-disable-next-line
         ['@media (max-width:780px)']: { 
-            display: "inline-block"
+            display: "inline-block",
             }
     },
     menuItem: {
@@ -31,7 +31,7 @@ const styles = makeStyles({
         flexGrow: 1,
         "&:hover": {
             color:  "#4f25c8"
-        },
+        },// eslint-disable-next-line
         ['@media (max-width:780px)']: { 
             paddingBottom: "1rem"    }
     }
@@ -41,8 +41,8 @@ function NavBar() {
     const classes = styles()
     return (
             <Toolbar position="sticky" color="rgba(0, 0, 0, 0.87)" className={classes.bar}>   
-                <img src={logo} className={classes.logo}/> 
-                <img src={logoMobile} className={classes.logoMobile}/> 
+                <img src={logo} className={classes.logo} alt={"logo"}/> 
+                <img src={logoMobile} className={classes.logoMobile} alt={"mobile-logo"}/> 
                 <Typography variant="h6" className={classes.menuItem}>
                    About
                 </Typography>
